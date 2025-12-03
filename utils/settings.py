2e0e38c5d0f3d50082b7051ffc7d81dc4fa70f82
Alex <sashaiva314@gmail.com>
2025-12-03 23:31:09 +0300

    fix: изменена точка входа

--- utils/settings.py
+++ utils/settings.py
@@ -3,9 +3,9 @@ BOT_TOKEN = "8350225727:AAHgYJPlKJEDyNzoTCCYaCOuR3W_mWklmJM"
 # Настройки SMTP сервера
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 587
-SMTP_USER = "sashaiva314@gmail.com"
-SMTP_PASSWORD = "uzon aivr isrt hwwr"
-FROM_EMAIL = "sashaiva314@gmail.com"
+SMTP_USER = "lidiafisher123@gmail.com"
+SMTP_PASSWORD = "slkk oxya eluc lika"
+FROM_EMAIL = "lidiafisher123@gmail.com"
 
 # ==== Database ====
 DB_PATH = "finlove.db"
