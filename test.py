398150de1bf7518c999e7ff968e968dce8657caf
Alex <sashaiva314@gmail.com>
2025-12-03 23:41:44 +0300

    feat: добавление зависимостей и исправление теста

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from email.mime.text import MIMEText
 
 msg = MIMEText("Тестовое письмо от FinLove бота", "plain", "utf-8")
 msg["Subject"] = "FinLove Test"
-msg["From"] = "sashaiva314@gmail.com"
-msg["To"] = "sashaiva314@gmail.com"
+msg["From"] = "lidiafisher123@gmail.com"
+msg["To"] = "lidiafisher123@gmail.com"
 
 server = smtplib.SMTP("smtp.gmail.com", 587, timeout=30)
 server.set_debuglevel(1)  # печать всего диалога с сервером
 
 server.starttls()
-server.login("sashaiva314@gmail.com", "uzon aivr isrt hwwr")
+server.login("lidiafisher123@gmail.com", "slkk oxya eluc lika")
 server.send_message(msg)
 server.quit()
